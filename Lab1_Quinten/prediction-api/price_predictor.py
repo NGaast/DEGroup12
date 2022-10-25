aa51bbae091ccf25dc1c6eaa068a5269f13bb38a
QTulp10 <114165555+QTulp10@users.noreply.github.com>
2022-10-25 19:23:03 +0200

    Update price_predictor.py

--- Lab1_Quinten/prediction-api/price_predictor.py
+++ Lab1_Quinten/prediction-api/price_predictor.py
@@ -1,5 +1,8 @@
 import json
 import os
+import logging
+import sys
+import joblib
 
 import pandas as pd
 from flask import jsonify
@@ -10,11 +13,17 @@ class PricePredictor:
 
     def predict_single_record(self, prediction_input):
         print(prediction_input)
-        # hier moet model nog in komen zodat ie kan predicten
+        # Download LR model
+        client = storage.client(project=project_id)
+        bucket = client.get_bucket(model_repo)
+        blob = bucket.blob('lr_model.pk1')
+        blob.download_to_filename('/tmp/local_lr_model.pk1')
+        # Load RandomForestRegressor model
+        model = joblib.load('/tmp/local_lr_model.pk1')
         print(json.dumps(prediction_input))
         df = pd.read_json(json.dumps(prediction_input), orient='records')
         print(df)
-        y_pred = self.model.predict(df)
+        y_pred = model.predict(df)
         print(y_pred[0])
         status = (y_pred[0] > 0.5)
         print(type(status[0]))
