e7bf61f38724fb689fc43e16b72e4f4159fbbc04
NGaastra <niels.gaastra@hotmail.com>
2022-10-26 17:03:01 +0200

    Added json store feature

--- data_storing/app.py
+++ data_storing/app.py
@@ -1,3 +1,5 @@
+from resources.data_storing import DataStoring
+
 from flask import Flask, jsonify, Response, request
 
 app=Flask(__name__)
@@ -5,11 +7,9 @@ app.config["DEBUG"] = True
 
 @app.route('/post_json', methods=['POST'])
 def store_data():
-    content_type = request.headers.get('Content-Type')
-    if (content_type == 'application/json'):
-        json = request.json
-        return json
+    if DataStoring.store_posted_data:
+        return "Data stored succesfully"
     else:
-        return 'Content-Type not supported!'
+        return "Content type not supported"
 
 app.run(host='0.0.0.0', port=5002)
