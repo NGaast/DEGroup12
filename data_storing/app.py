e608ef1acbaa8b509de49a83a48884c35fb8e6d1
NGaastra <niels.gaastra@hotmail.com>
2022-10-26 16:35:26 +0200

    Added functionality to data_storing

--- data_storing/app.py
+++ data_storing/app.py
@@ -1,20 +1,17 @@
 from resources.Data_storing import Database
-from flask import Flask, jsonify, Response
+from flask import Flask, jsonify, Response, request
 
 
 app=Flask(__name__)
 db = Database()
 
-@app.route('/store-data', methods=['POST'])
+@app.route('/post_json', methods=['POST'])
 def store_data():
-    db.store_tables()
-    return jsonify({'message' : 'Data stored successfully!'}), 200
-
-@app.route('/read-data/<table_name>', methods=['GET'])
-def read_data(table_name):
-    return Response(db.read_table(table_name).to_json(orient='records'), status=200, mimetype='application/json')
-
-with app.app_context():
-    store_data()
-
+    content_type = request.headers.get('Content-Type')
+    if (content_type == 'application/json'):
+        json = request.json
+        return json
+    else:
+        return 'Content-Type not supported!'
+        
 app.run(host='0.0.0.0', port=5000)
