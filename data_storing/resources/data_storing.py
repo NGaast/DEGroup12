0e0ce0c75dc3a85c955dc9f1ddc7e6748d218217
NGaastra <niels.gaastra@hotmail.com>
2022-10-26 17:23:00 +0200

    Attempt to fix json import

--- data_storing/resources/data_storing.py
+++ data_storing/resources/data_storing.py
@@ -11,7 +11,7 @@ class DataStoring:
         content_type = request.headers.get('Content-Type')
         if (content_type == 'application/json'):
             json_post = request.get_json()
-            df = pd.read_json(json_post)
+            df = pd.read_json(json_post, orient='records')
             # Save to GCS as lr_model.pkl
             client = storage.Client(project="de-2022-ng")
             bucket = client.get_bucket("model_repo_de2022_ng")
