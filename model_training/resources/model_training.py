5ee6ab605b0570c682c70b09c293707e15d77d06
NGaastra <niels.gaastra@hotmail.com>
2022-10-29 16:20:42 +0200

    Invoke build

--- model_training/resources/model_training.py
+++ model_training/resources/model_training.py
@@ -24,10 +24,12 @@ class ModelManaging:
         if (content_type == 'application/json'):
             json_post = request.get_json()
             df = pd.DataFrame.from_dict(json_post)
+            # Drop NaN values
+            df = df.dropna()
             # Save to GCS as lr_model.pkl
             client = storage.Client(project="de-2022-ng")
             bucket = client.get_bucket("data_de2022_ng")
-            blob = bucket.blob('dataset_test.csv')
+            blob = bucket.blob('dataset.csv')
             # Upload the locally saved model
             blob.upload_from_string(df.to_csv(), content_type='application/json')
             return True
--- prediction-ui/app.py
+++ prediction-ui/app.py
@@ -18,12 +18,12 @@ def predict_price():
     if request.method == "POST":
         prediction_input = [
             {
-                "crim": float(request.form.get("crim")),  # getting input with name = crim in HTML form
-                "nox": float(request.form.get("nox")),  # getting input with name = nox in HTML form
-                "rm": int(request.form.get("rm")),
-                "dis": float(request.form.get("dis")),
-                "ptratio": float(request.form.get("ptratio")),
-                "lstat": float(request.form.get("lstat"))
+                "CRIM": float(request.form.get("crim")),  # getting input with name = crim in HTML form
+                "NOX": float(request.form.get("nox")),  # getting input with name = nox in HTML form
+                "RM": int(request.form.get("rm")),
+                "DIS": float(request.form.get("dis")),
+                "PTRATIO": float(request.form.get("ptratio")),
+                "LSTAT": float(request.form.get("lstat"))
             }
         ]
         print(prediction_input, file=sys.stdout)
@@ -31,7 +31,6 @@ def predict_price():
         # localhost or 127.0.0.1 is used when the applications are on the same machine.
         api_url = os.environ['PREDICTION_API_URL']
         api_port = os.environ['PREDICTION_API_PORT']
-        request_url_with_port = "{0}:{1}/price_predictor".format(api_url,api_port)
         request_url = "{0}/price_predictor".format(api_url)
         res = requests.post(request_url, json=json.loads(json.dumps(prediction_input)))
         print(res.status_code, file=sys.stdout)
