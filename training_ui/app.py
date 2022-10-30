8f7fe612228189779c2a032f6b1b0ed4b334b998
NGaastra <niels.gaastra@hotmail.com>
2022-10-30 16:52:49 +0100

    Added training endpoint

--- training_ui/app.py
+++ training_ui/app.py
@@ -14,7 +14,7 @@ app.secret_key = secret
 
 @app.route('/training_ui', methods=['GET', 'POST'])
 def training_ui():
-    if request.method == 'POST':
+    if request.method == "POST":
         # No file in request
         if 'training_data' not in request.files:
             flash("No file in request")
@@ -32,7 +32,16 @@ def training_ui():
         upload_url = "{0}/{1}".format(request_path, upload_endpoint)
         train_url = "{0}/{1}".format(request_path, train_endpoint)
         upload_request = requests.post(upload_url, json=json.load(data_file))
+        return upload_request.json()
     return render_template("training_template.html")  # this method is called of HTTP method is GET, e.g., when browsing the link
     
+@app.route('/train_model', methods=['POST'])
+def train_model():
+    if request.method == "POST":
+        request_path = os.environ['TRAINING_API']
+        train_endpoint = os.environ['TRAIN_ENDPOINT']
+        train_url = "{0}/{1}".format(request_path, train_endpoint)
+        upload_request = requests.post(upload_url)
+        return upload_request.json()
 
 app.run(host='0.0.0.0', port=5000)
