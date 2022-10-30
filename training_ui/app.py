c5c4b2878517176657e6979d99cf799c68b5097b
NGaastra <niels.gaastra@hotmail.com>
2022-10-30 17:05:38 +0100

    Added upload to training_ui post method

--- training_ui/app.py
+++ training_ui/app.py
@@ -14,7 +14,28 @@ app.secret_key = secret
 
 @app.route('/training_ui', methods=['GET', 'POST'])
 def training_ui():
+    if request.method == "POST":
+        # No file in request
+        if 'training_data' not in request.files:
+            flash("No file in request")
+            return redirect(request.url)
+        # Retrieve file
+        data_file = request.files['training_data']
+        # No file
+        if data_file.filename == '':
+            flash("No file selected")
+            return redirect(request.url)
+
+        request_path = os.environ['TRAINING_API']
+        upload_endpoint = os.environ['UPLOAD_ENDPOINT']
+        upload_url = "{0}/{1}".format(request_path, upload_endpoint)
+        upload_request = requests.post(upload_url, json=json.load(data_file))
+        return upload_request.json()
     return render_template("training_template.html")  # this method is called of HTTP method is GET, e.g., when browsing the link
+
+# @app.route('/upload_data', methods=['POST'])
+# def upload_data():
+
     
 @app.route('/train_model', methods=['POST'])
 def train_model():
@@ -24,25 +45,5 @@ def train_model():
         train_url = "{0}/{1}".format(request_path, train_endpoint)
         train_request = requests.post(train_url)
         return train_request.json()
-
-@app.route('/upload_data', methods=['POST'])
-def upload_data():
-    if request.method == "POST":
-            # No file in request
-            if 'training_data' not in request.files:
-                flash("No file in request")
-                return redirect(request.url)
-            # Retrieve file
-            data_file = request.files['training_data']
-            # No file
-            if data_file.filename == '':
-                flash("No file selected")
-                return redirect(request.url)
-
-            request_path = os.environ['TRAINING_API']
-            upload_endpoint = os.environ['UPLOAD_ENDPOINT']
-            upload_url = "{0}/{1}".format(request_path, upload_endpoint)
-            upload_request = requests.post(upload_url, json=json.load(data_file))
-            return upload_request.json()
 
 app.run(host='0.0.0.0', port=5000)
